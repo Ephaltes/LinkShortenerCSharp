c96cd7a80cbba051dff81beba3e4130a1506eae2
Ngoc Lam Nguyen <if19b072@technikum-wien.at>
2021-02-13 19:38:56 +0100

    initial

---
+++ LinkShortener/Controllers/LinkController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LinkShortener.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LinkController : ControllerBase
+    {
+        private readonly ILogger<LinkController> _logger;
+
+        public LinkController(ILogger<LinkController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("{sluge=}")]
+        public string Get(string sluge)
+        {
+            if (string.IsNullOrWhiteSpace(sluge))
+                return "empty";
+
+            return sluge;
+        }
+    }
+}
